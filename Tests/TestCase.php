93fc491c6aef2ef4993f82f6079d78b6ae867845
Deji69 <the_zone@hotmail.co.uk>
2020-01-06 21:43:13 +0000

    Isolate Matching as component

--- Tests/TestCase.php
+++ Tests/TestCase.php
@@ -1,13 +1,8 @@
 <?php
 namespace Encase\Matching\Tests;
 
-use Mockery;
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-	public function tearDown(): void
-	{
-		Mockery::close();
-	}
 }
