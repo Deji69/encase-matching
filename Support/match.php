3909bb41623ecdad10164b08fb8ed17f4f28b26c
Deji69 <the_zone@hotmail.co.uk>
2020-01-09 02:03:20 +0000

    Better document match()

--- Support/match.php
+++ Support/match.php
@@ -6,8 +6,12 @@ use Encase\Matching\Matcher;
 /**
  * Matches arguments against a set of cases.
  *
+ * If `$cases` is a Matcher object (e.g. built with pattern()), that matcher is
+ * invoked on `$value` and the result is returned. Otherwise `$cases` will be
+ * used to build the Matcher to invoke.
+ *
  * @param  mixed $value Value to match.
- * @param  array $cases `When` cases to match against.
+ * @param  array|Matcher $cases `When` cases to match against.
  * @return mixed Result of matched case.
  */
 function match($value, $cases = null)
